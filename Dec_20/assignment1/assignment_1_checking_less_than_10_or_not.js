9dbf17573300d56c95de6902d418ecfde186c329
Mukul Paul <94905659+mukulpaul2105@users.noreply.github.com>
2022-01-01 18:41:11 +0530

    Add files via upload

---
+++ Dec_20/assignment1/assignment_1_checking_less_than_10_or_not.js
@@ -0,0 +1,19 @@
+var num = prompt("Enter the Number");
+
+num = parseInt(num);
+
+if(num < 10)
+{
+    console.log("Number is less than 10");
+    alert("Number is less than 10");
+}
+else if(num > 10)
+{
+    console.log("Number is greater than 10");
+    alert("Number is less greater 10");
+}
+else
+{
+    console.log("Number is equal to 10");
+    alert("Number is equal to 10");
+}
