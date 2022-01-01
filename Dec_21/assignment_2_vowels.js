a4e43b6a902b97e911a8e008a02176f432433f6f
Mukul Paul <94905659+mukulpaul2105@users.noreply.github.com>
2022-01-01 19:15:14 +0530

    Add files via upload

---
+++ Dec_21/assignment_2_vowels.js
@@ -0,0 +1,28 @@
+// Whether the is vowel or not
+
+var char = prompt("Enter any character");
+if(char == "a") {
+	alert("It is a vowel");
+} else if(char == "e") {
+	alert("It is a vowel");
+} else if(char == "i") {
+	alert("It is a vowel");
+} else if(char == "o") {
+	alert("It is a vowel");
+} else if(char == "u") {
+	alert("It is a vowel");
+} else if(char == "A") {
+	alert("It is a vowel");
+} else if(char == "E") {
+	alert("It is a vowel");
+} else if(char == "I") {
+	alert("It is a vowel");
+} else if(char == "O") {
+	alert("It is a vowel");
+} else if(char == "U") {
+	alert("It is a vowel");
+} else  {
+	alert("It is Consonent");
+}
+
+
